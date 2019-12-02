da1d88bcdad67df759d14d7592034bd42c32c8a6
Beksultan Kadyrov <kadyrovba1@yandex.ru>
2019-12-02 20:34:51 +0300

    output products on orders/

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import orders
 
 urlpatterns = [
-    path('', index),
+    #path('', index),
+    path('orders/', orders, name='orders'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product
 
 
-def index(request):
-    return HttpResponse('Your are welcom!')
+
+def orders(request):
+    products = Product.objects.order_by('-create_time')[:3]
+    product = {'products': products}
+    return render(request, 'users/orders.html', product)
+
